4866bb020c1d894f650b9163395c73086bcd56f8
Meltryllis215 <3223347993@qq.com>
2024-11-19 21:38:58 +0800

    test

---
+++ 设计模式.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long max = LLONG_MAX;
+    long long min = LLONG_MIN;
+    unsigned long long ullMax = ULLONG_MAX;
+
+    cout << "Max Long Long value: " << max << endl
+        << "Min Long Long value: " << min << endl
+        << "Max unsigned Long Long value: " << ullMax << endl;
+    return 0;
+}
+
